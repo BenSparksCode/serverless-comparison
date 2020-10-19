bc6c787cd8926c502f61417032cc445b783e3ede
Ben Sparks <bensparks0@gmail.com>
2020-10-19 18:12:27 +0200

    Experimenting with AWS Lambda, TypeScript, Axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios, {AxiosRequestConfig} from "axios";
+
+const API_KEY = process.env.REACT_APP_FACTORIAL_API
+
 function App() {
+
+  
+
+  async function callApi(){
+
+
+    const config:AxiosRequestConfig = {
+      method: 'post',
+      url: 'https://9azeazmu7i.execute-api.us-east-1.amazonaws.com/default/factorial',
+      data: {
+        num: 5
+      },
+      headers: {
+        'x-api-key': API_KEY
+      },
+    }
+
+    console.log(config);
+
+    const res = await axios(config)
+
+
+
+    console.log(res);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +40,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <button onClick={callApi}>TEST API</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
