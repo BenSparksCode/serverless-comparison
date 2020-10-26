d3ef87446ff2afd64f572848bb8e4ddbc7695bd6
Ben Sparks <bensparks0@gmail.com>
2020-10-26 19:48:53 +0200

    Added Styled Components and Ant Design

---
+++ src/views/MainView.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// const MainView = styled.button`
+
+// `
+
+interface MainViewProps {
+
+}
+
+export const MainView: React.FC<MainViewProps> = ({}) => {
+        return (
+            <div>
+
+            </div>
+        );
+}
